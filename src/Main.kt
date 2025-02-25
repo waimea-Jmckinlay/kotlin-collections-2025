6f25b0555c575d888b4ea0b093fe3a9c5bd6627f
waimea-Jmckinlay <jcmckinlay@waimea.school.nz>
2025-02-25 13:24:55 +1300

    made some lists and played around with them

--- src/Main.kt
+++ src/Main.kt
@@ -4,6 +4,60 @@
  */
 
 fun main() {
-    println("Hello World!")
+    println("kotiln mutableList")
+    //create our list
+    val snacks = mutableListOf<String>()
+
+    //show it
+    println(snacks)
+
+
+
+    //add in some things
+    snacks.add("cheetos")
+    snacks.add("carrot")
+    snacks.add("pizza")
+    snacks.add("pineapple")
+    snacks.add("cheese")
+    snacks.add("cookey dough ")
+    println(snacks)
+
+    //show specifices snacke
+
+    println(snacks[0])
+   // println(snacks[6])
+    //this would be out of bounds
+
+    //change a value
+    snacks[1] = "dog food"
+    println(snacks)
+
+    //removeitems
+    snacks.removeAt(0)
+
+    println(snacks)
+
+    snacks.add(1,"Baconstrings")
+    println(snacks)
+
+    //short the list
+    snacks.sort()
+    println(snacks)
+
+    //shuffle the list (random)
+    snacks.shuffle()
+    println(snacks)
+
+
+    //get a random item
+
+    val item = snacks.random()
+    println(item)
+
+    //loop though the list
+    for (snack in snacks) {
+        println(snack)
+    }
+
 }
 
